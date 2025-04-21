0965158e7a1bdbfeef12e42f7c5238ac888877e9
Xavier Salazar <x.salazar1729@gmail.com>
2025-04-21 11:25:49 -0700

    Skeleton testing

--- src/Components/body.tsx
+++ src/Components/body.tsx
@@ -1,10 +1,17 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
 export default function Body() {
   return (
     <Container
-      maxWidth="xl"
+      maxWidth="sm"
       sx={{
         display: "flex",
         justifyContent: "center",
@@ -12,9 +19,49 @@ export default function Body() {
         flexGrow: "1",
       }}
     >
-      <Typography variant="body1" align="center">
-        👋 This is a basic Typescript React template!
-      </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container size={2} direction="column" spacing={2}>
+          <Grid>
+            <Card variant="outlined">
+              <CardMedia
+                sx={{ height: "64px" }}
+                image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Southeast_Portland%2C_Oregon_%28January_23%2C_2021%29_-_082.jpg/2560px-Southeast_Portland%2C_Oregon_%28January_23%2C_2021%29_-_082.jpg"
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  North Elevator
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  Status: Open
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button>Update Status</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid>
+            <Card variant="outlined">
+              <CardMedia
+                sx={{ height: "64px" }}
+                image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Southeast_Portland%2C_Oregon_%28January_23%2C_2021%29_-_082.jpg/2560px-Southeast_Portland%2C_Oregon_%28January_23%2C_2021%29_-_082.jpg"
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  South Elevator
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  Status: Closed
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button>Update Status</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
 }
